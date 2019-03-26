f11a8c1656087ae394472c5c3d11286402e5722a
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-26 16:43:29 +0300

    [Joiner] Setup publishing

--- build.py
+++ build.py
@@ -1,8 +1,21 @@
 import os
 import subprocess
+import reactor
 
 def runWithCWD(cmd, cwd):
-	cwd = (os.getcwd() + "/"+ cwd).replace('/', '\\')
-	subprocess.call(cmd, cwd=cwd)
+    cwd = f"{os.getcwd()}/{cwd}".replace('/', '\\')
+    subprocess.call(cmd, cwd=cwd)
 
-runWithCWD("dotnet publish -c Release --self-contained --no-build --runtime win-x64", "SteamFakePlayer")
+
+runWithCWD("dotnet publish -c Release --self-contained --runtime win-x64 --no-dependencies", "SteamFakePlayer")
+
+reactor.process("Joiner", "SteamFakePlayer", [
+    "Facepunch.Network.dll",
+    "Facepunch.System.dll",
+    "Assembly-CSharp.dll",
+    "UnityEngine.CoreModule.dll",
+    "SapphireEngine.dll",
+    "Rust.Data.dll",
+    "Sentry.dll",
+    "Sentry.Protocol.dll"
+])
