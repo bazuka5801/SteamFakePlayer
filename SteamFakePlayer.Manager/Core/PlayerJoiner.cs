4b97d2a74e296e5638c1d2993077d578c394f319
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-27 00:20:28 +0300

    [Manager] [PlayerJoiner] Track player states

--- SteamFakePlayer.Manager/Core/PlayerJoiner.cs
+++ SteamFakePlayer.Manager/Core/PlayerJoiner.cs
@@ -1,25 +1,60 @@
-﻿using SteamFakePlayer.Manager.Data;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SteamFakePlayer.Manager.Data;
 
 namespace SteamFakePlayer.Manager.Core
 {
-    internal class PlayerJoiner
+    internal enum ConnectionState
     {
-        private BotAccountData _account;
-        private ServerData _server;
+        ConnectingToSteam,
+        LaunchingRust,
+        Joining,
+        Playing,
+        Disconnected
+    }
+
+    internal delegate void StateChanged(ConnectionState state);
+
+    internal abstract class PlayerJoiner
+    {
+        private readonly BotAccountData _account;
+        private Process _joinerProcess;
 
         private bool _running;
+        private readonly ServerData _server;
+
+        private ConnectionState state = ConnectionState.Disconnected;
 
         public PlayerJoiner(BotAccountData account, ServerData server)
         {
             _account = account;
             _server = server;
+        }
+
+        internal ConnectionState State
+        {
+            get => state;
+            set
+            {
+                if (state != value)
+                {
+                    state = value;
+                    OnStateChanged(state);
+                }
+            }
+        }
+
+        internal event StateChanged StateChanged;
+
+        protected virtual void OnStateChanged(ConnectionState state)
+        {
+            if (StateChanged != null)
+            {
+                StateChanged(state);
+            }
         }
 
         public void Join()
@@ -36,9 +71,8 @@ namespace SteamFakePlayer.Manager.Core
         private void RunJoiner()
         {
             ProcessStartInfo processInfo;
-            Process process;
-            
-            string args = GenerateJoinerArguments();
+
+            var args = GenerateJoinerArguments();
 
             processInfo = new ProcessStartInfo(DataManager.Data.JoinerFile, args)
             {
@@ -49,19 +83,64 @@ namespace SteamFakePlayer.Manager.Core
                 RedirectStandardOutput = true,
                 WorkingDirectory = Path.GetDirectoryName(DataManager.Data.JoinerFile)
             };
-            
-            process = Process.Start(processInfo);
-            process.WaitForExit();
 
-            // *** Read the streams ***
-            string output = process.StandardOutput.ReadToEnd();
-            string error = process.StandardError.ReadToEnd();
+            _joinerProcess = Process.Start(processInfo);
+            _joinerProcess.OutputDataReceived += Joiner_OutputDataReceived;
+            _joinerProcess.ErrorDataReceived += Joiner_ErrorDataReceived;
+            _joinerProcess.BeginOutputReadLine();
+            _joinerProcess.BeginErrorReadLine();
+            _joinerProcess.WaitForExit();
+
+            OnDisconnectedFromServer("Quiting...");
 
 
             _running = false;
         }
 
-        private string GenerateJoinerArguments()
+        private void Joiner_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Data);
+        }
+
+        internal void Disconnect()
+        {
+            if (State != ConnectionState.Disconnected)
+            {
+                _joinerProcess.Kill();
+            }
+        }
+
+        protected virtual void OnConnectedToServer(string servername)
+        {
+            State = ConnectionState.Playing;
+        }
+
+        protected virtual void OnDisconnectedFromServer(string reason)
+        {
+            State = ConnectionState.Disconnected;
+        }
+
+        private void Joiner_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data == null)
+            {
+                return;
+            }
+
+            if (e.Data.Contains("Подключились к: "))
+            {
+                var servername = e.Data.Split(new[] {"Подключились к: "}, StringSplitOptions.RemoveEmptyEntries)[1];
+                OnConnectedToServer(servername);
+            }
+            else if (e.Data.Contains("Соеденение с игровым сервером разорвано: Connection Attempt Failed"))
+            {
+                OnDisconnectedFromServer("Connection Attempt Failed");
+            }
+
+            Console.WriteLine(e.Data);
+        }
+
+        protected virtual string GenerateJoinerArguments()
         {
             return $"\"{_account.Username}\" " +
                    $"\"{_account.Password}\" " +
