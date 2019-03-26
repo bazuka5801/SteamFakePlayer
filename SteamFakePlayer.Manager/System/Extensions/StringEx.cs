aab6079fd120dff7edff931ea3c8ecd384d9e2bb
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-27 00:15:52 +0300

    [Manager] Add StringEx

---
+++ SteamFakePlayer.Manager/System/Extensions/StringEx.cs
@@ -0,0 +1,15 @@
+﻿namespace SteamFakePlayer.Manager.Extensions
+{
+    public static class StringEx
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (!string.IsNullOrEmpty(value) && value.Length > maxLength)
+            {
+                return value.Substring(0, maxLength) + "...";
+            }
+
+            return value;
+        }
+    }
+}
