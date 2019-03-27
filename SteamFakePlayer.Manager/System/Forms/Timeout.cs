0efaf2c1e66ed0d3aea8a4c1c96d0aa869020a52
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-27 22:34:14 +0300

    [Manager] [System] Add Timeout class

---
+++ SteamFakePlayer.Manager/System/Forms/Timeout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SteamFakePlayer.Manager
+{
+    internal class Timeout : System.Timers.Timer
+    {
+        public Timeout(double delay, Action action)
+        {
+            this.AutoReset = false;
+            this.Interval = delay;
+            this.Elapsed += (sender, args) => action();
+            this.Start();
+        }
+    }
+}
