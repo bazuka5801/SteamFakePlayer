4da148e2c2b48e4a6f8b380c9618f0ae4de7d03f
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-27 22:33:58 +0300

    [Manager] [System] Add Rand class

---
+++ SteamFakePlayer.Manager/System/Rand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SteamFakePlayer.Manager
+{
+    public static class Rand
+    {
+        private static Random rand = new Random();
+
+        public static Int32 Int32(Int32 min, Int32 max)
+        {
+            return rand.Next(min, max);
+        }
+    }
+}
