6e20c0becd6841c32a72e913cb2cab05b331a6a0
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-27 21:48:07 +0300

    [Manager] [BotOptionsForm] Coplete UI/Logic

--- SteamFakePlayer.Manager/Forms/BotOptionsForm.Designer.cs
+++ SteamFakePlayer.Manager/Forms/BotOptionsForm.Designer.cs
@@ -30,22 +30,22 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BotOptionsForm));
             this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
-            this.tlpEnter = new System.Windows.Forms.TableLayoutPanel();
-            this.lblEnter = new System.Windows.Forms.Label();
-            this.tbEntetMax = new System.Windows.Forms.TextBox();
-            this.tlpButtons = new System.Windows.Forms.TableLayoutPanel();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.btnSave = new System.Windows.Forms.Button();
-            this.lblHelp = new System.Windows.Forms.Label();
-            this.tbEnterMin = new System.Windows.Forms.TextBox();
             this.tlpExit = new System.Windows.Forms.TableLayoutPanel();
             this.tbExitMin = new System.Windows.Forms.TextBox();
             this.lblExit = new System.Windows.Forms.Label();
             this.tbExitMax = new System.Windows.Forms.TextBox();
+            this.tlpButtons = new System.Windows.Forms.TableLayoutPanel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tlpEnter = new System.Windows.Forms.TableLayoutPanel();
+            this.tbEnterMin = new System.Windows.Forms.TextBox();
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.tbEnterMax = new System.Windows.Forms.TextBox();
+            this.lblHelp = new System.Windows.Forms.Label();
             this.tlpMain.SuspendLayout();
-            this.tlpEnter.SuspendLayout();
-            this.tlpButtons.SuspendLayout();
             this.tlpExit.SuspendLayout();
+            this.tlpButtons.SuspendLayout();
+            this.tlpEnter.SuspendLayout();
             this.SuspendLayout();
             // 
             // tlpMain
@@ -67,101 +67,6 @@
             this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlpMain.Size = new System.Drawing.Size(513, 173);
             this.tlpMain.TabIndex = 3;
-            // 
-            // tlpEnter
-            // 
-            this.tlpEnter.ColumnCount = 3;
-            this.tlpEnter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
-            this.tlpEnter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlpEnter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlpEnter.Controls.Add(this.tbEnterMin, 0, 0);
-            this.tlpEnter.Controls.Add(this.lblEnter, 0, 0);
-            this.tlpEnter.Controls.Add(this.tbEntetMax, 1, 0);
-            this.tlpEnter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tlpEnter.Location = new System.Drawing.Point(0, 60);
-            this.tlpEnter.Margin = new System.Windows.Forms.Padding(0);
-            this.tlpEnter.Name = "tlpEnter";
-            this.tlpEnter.RowCount = 1;
-            this.tlpEnter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpEnter.Size = new System.Drawing.Size(513, 34);
-            this.tlpEnter.TabIndex = 1;
-            // 
-            // lblEnter
-            // 
-            this.lblEnter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblEnter.Location = new System.Drawing.Point(3, 0);
-            this.lblEnter.Name = "lblEnter";
-            this.lblEnter.Size = new System.Drawing.Size(74, 34);
-            this.lblEnter.TabIndex = 0;
-            this.lblEnter.Text = "ВХОД";
-            this.lblEnter.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // tbEntetMax
-            // 
-            this.tbEntetMax.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbEntetMax.Location = new System.Drawing.Point(299, 3);
-            this.tbEntetMax.Name = "tbEntetMax";
-            this.tbEntetMax.Size = new System.Drawing.Size(211, 27);
-            this.tbEntetMax.TabIndex = 1;
-            // 
-            // tlpButtons
-            // 
-            this.tlpButtons.ColumnCount = 2;
-            this.tlpButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlpButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlpButtons.Controls.Add(this.btnCancel, 1, 0);
-            this.tlpButtons.Controls.Add(this.btnSave, 0, 0);
-            this.tlpButtons.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tlpButtons.Location = new System.Drawing.Point(0, 128);
-            this.tlpButtons.Margin = new System.Windows.Forms.Padding(0);
-            this.tlpButtons.Name = "tlpButtons";
-            this.tlpButtons.RowCount = 1;
-            this.tlpButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlpButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlpButtons.Size = new System.Drawing.Size(513, 45);
-            this.tlpButtons.TabIndex = 0;
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnCancel.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnCancel.ForeColor = System.Drawing.Color.Red;
-            this.btnCancel.Location = new System.Drawing.Point(259, 3);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(251, 39);
-            this.btnCancel.TabIndex = 1;
-            this.btnCancel.Text = "Отмена";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            // 
-            // btnSave
-            // 
-            this.btnSave.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnSave.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnSave.ForeColor = System.Drawing.Color.ForestGreen;
-            this.btnSave.Location = new System.Drawing.Point(3, 3);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(250, 39);
-            this.btnSave.TabIndex = 0;
-            this.btnSave.Text = "Сохранить";
-            this.btnSave.UseVisualStyleBackColor = true;
-            // 
-            // lblHelp
-            // 
-            this.lblHelp.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblHelp.Location = new System.Drawing.Point(3, 0);
-            this.lblHelp.Name = "lblHelp";
-            this.lblHelp.Size = new System.Drawing.Size(507, 60);
-            this.lblHelp.TabIndex = 4;
-            this.lblHelp.Text = "Все значения устанавливаются в секундах\r\nЕсли 2 поля, то это минимум и максимум.";
-            this.lblHelp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // tbEnterMin
-            // 
-            this.tbEnterMin.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbEnterMin.Location = new System.Drawing.Point(83, 3);
-            this.tbEnterMin.Name = "tbEnterMin";
-            this.tbEnterMin.Size = new System.Drawing.Size(210, 27);
-            this.tbEnterMin.TabIndex = 2;
             // 
             // tlpExit
             // 
@@ -207,6 +112,103 @@
             this.tbExitMax.Size = new System.Drawing.Size(211, 27);
             this.tbExitMax.TabIndex = 1;
             // 
+            // tlpButtons
+            // 
+            this.tlpButtons.ColumnCount = 2;
+            this.tlpButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpButtons.Controls.Add(this.btnCancel, 1, 0);
+            this.tlpButtons.Controls.Add(this.btnSave, 0, 0);
+            this.tlpButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpButtons.Location = new System.Drawing.Point(0, 128);
+            this.tlpButtons.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpButtons.Name = "tlpButtons";
+            this.tlpButtons.RowCount = 1;
+            this.tlpButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tlpButtons.Size = new System.Drawing.Size(513, 45);
+            this.tlpButtons.TabIndex = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCancel.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancel.ForeColor = System.Drawing.Color.Red;
+            this.btnCancel.Location = new System.Drawing.Point(259, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(251, 39);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSave.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnSave.Location = new System.Drawing.Point(3, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(250, 39);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tlpEnter
+            // 
+            this.tlpEnter.ColumnCount = 3;
+            this.tlpEnter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tlpEnter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpEnter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpEnter.Controls.Add(this.tbEnterMin, 0, 0);
+            this.tlpEnter.Controls.Add(this.lblEnter, 0, 0);
+            this.tlpEnter.Controls.Add(this.tbEnterMax, 1, 0);
+            this.tlpEnter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpEnter.Location = new System.Drawing.Point(0, 60);
+            this.tlpEnter.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpEnter.Name = "tlpEnter";
+            this.tlpEnter.RowCount = 1;
+            this.tlpEnter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpEnter.Size = new System.Drawing.Size(513, 34);
+            this.tlpEnter.TabIndex = 1;
+            // 
+            // tbEnterMin
+            // 
+            this.tbEnterMin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbEnterMin.Location = new System.Drawing.Point(83, 3);
+            this.tbEnterMin.Name = "tbEnterMin";
+            this.tbEnterMin.Size = new System.Drawing.Size(210, 27);
+            this.tbEnterMin.TabIndex = 2;
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblEnter.Location = new System.Drawing.Point(3, 0);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(74, 34);
+            this.lblEnter.TabIndex = 0;
+            this.lblEnter.Text = "ВХОД";
+            this.lblEnter.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tbEntetMax
+            // 
+            this.tbEnterMax.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbEnterMax.Location = new System.Drawing.Point(299, 3);
+            this.tbEnterMax.Name = "tbEntetMax";
+            this.tbEnterMax.Size = new System.Drawing.Size(211, 27);
+            this.tbEnterMax.TabIndex = 1;
+            // 
+            // lblHelp
+            // 
+            this.lblHelp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblHelp.Location = new System.Drawing.Point(3, 0);
+            this.lblHelp.Name = "lblHelp";
+            this.lblHelp.Size = new System.Drawing.Size(507, 60);
+            this.lblHelp.TabIndex = 4;
+            this.lblHelp.Text = "Все значения устанавливаются в секундах\r\nЕсли 2 поля, то это минимум и максимум.";
+            this.lblHelp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // BotOptionsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
@@ -223,11 +225,11 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Настройки бота";
             this.tlpMain.ResumeLayout(false);
-            this.tlpEnter.ResumeLayout(false);
-            this.tlpEnter.PerformLayout();
-            this.tlpButtons.ResumeLayout(false);
             this.tlpExit.ResumeLayout(false);
             this.tlpExit.PerformLayout();
+            this.tlpButtons.ResumeLayout(false);
+            this.tlpEnter.ResumeLayout(false);
+            this.tlpEnter.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -245,7 +247,7 @@
         private System.Windows.Forms.TableLayoutPanel tlpEnter;
         private System.Windows.Forms.TextBox tbEnterMin;
         private System.Windows.Forms.Label lblEnter;
-        private System.Windows.Forms.TextBox tbEntetMax;
+        private System.Windows.Forms.TextBox tbEnterMax;
         private System.Windows.Forms.Label lblHelp;
     }
 }
--- SteamFakePlayer.Manager/Forms/BotOptionsForm.cs
+++ SteamFakePlayer.Manager/Forms/BotOptionsForm.cs
@@ -1,20 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SteamFakePlayer.Manager
 {
     public partial class BotOptionsForm : Form
     {
-        public BotOptionsForm()
+        private readonly BotOptionsModel _model;
+
+        public BotOptionsForm(BotOptionsModel model)
         {
+            _model = model;
+
             InitializeComponent();
+
+            LoadData(model);
         }
+
+        public void LoadData(BotOptionsModel model)
+        {
+            tbEnterMin.Text = model.EnterMin.ToString();
+            tbEnterMax.Text = model.EnterMax.ToString();
+            tbExitMin.Text = model.ExitMin.ToString();
+            tbExitMax.Text = model.ExitMax.ToString();
+        }
+
+        public bool SaveData()
+        {
+            int enterMin = 0, enterMax = 0, exitMin = 0, exitMax = 0;
+            var result = ValidateIntField("EnterMin", tbEnterMin, out enterMin)
+                         && ValidateIntField("EnterMax", tbEnterMax, out enterMax)
+                         && ValidateIntField("ExitMin", tbExitMin, out exitMin)
+                         && ValidateIntField("ExitMax", tbExitMax, out exitMax);
+
+            if (result)
+            {
+                _model.EnterMin = enterMin;
+                _model.EnterMax = enterMax;
+                _model.ExitMin = exitMin;
+                _model.ExitMax = exitMax;
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ValidateIntField(string name, TextBox tb, out int result)
+        {
+            if (GetInt(tb, out result) == false)
+            {
+                MessageUtils.Error($"Поле {name} не является числом!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool GetInt(TextBox tb, out int result) => int.TryParse(tb.Text, out result);
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (SaveData())
+            {
+                _model.Success = true;
+                Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public class BotOptionsModel : DialogModel<BotOptionsModel, BotOptionsForm>
+    {
+        public int EnterMin, EnterMax;
+        public int ExitMin, ExitMax;
     }
 }
